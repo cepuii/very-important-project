f529b19e8f11cc6a49caf6aaece87dc5e64e1168
cepuii <cepuii13@gmail.com>
2022-06-22 21:13:43 +0300

    Update gitignore
    Update gitignore

--- src/main/java/edu/cepuii/App.java
+++ src/main/java/edu/cepuii/App.java
@@ -1,5 +1,10 @@
 package edu.cepuii;
 
+import edu.cepuii.work_with_files.FilesUtils;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String text = """
+            Welcome to Java
+            Добро пожаловать в Яву
+                        %s""".formatted(System.getProperty("user.name"));
+       // FilesUtils.writeFile("text.txt", text);
+        FilesUtils.readFile("text.txt").forEach(System.out::println);
+        try {
+            Files.deleteIfExists(Path.of("text.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/edu/cepuii/work_with_files/FilesUtils.java
@@ -0,0 +1,29 @@
+package edu.cepuii.work_with_files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+public class FilesUtils {
+
+  public static void writeFile(String filename, String text){
+    try {
+      Files.write(Paths.get(filename), Collections.singleton(text));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+  public static Stream<String> readFile(String filename){
+    try {
+      return Files.lines(Path.of(filename));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+}
